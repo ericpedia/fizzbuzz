421fde48853b3c2c021a7ca27d66030e58226b09
Eric <eric.policy@gmail.com>
2013-08-22 19:23:57 -0400

    It begins.

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+ * Make numbers fizz and buzz
+ *
+ * @author  Eric
+ * 
+ * @link http://en.wikipedia.org/wiki/Fizz_buzz
+ * @link http://lmgtfy.com/?q=fizz+buzz
+ * 
+ * @param  integer $min     The number at which to start
+ * @param  integer $max     The number at which to stop
+ * @param  array   $options Exciting options hitherto unknown!
+ * 
+ * @return Nothing to see here.
+ * 
+ */
+function fizzbuzz( $min = 1, $max = 100, $options = array() ) {
+
+
+
+}

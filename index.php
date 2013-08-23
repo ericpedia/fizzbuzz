c4e3af765415ab00345cec88b5f75528f1fcb609
Eric <eric.policy@gmail.com>
2013-08-22 20:46:10 -0400

    It's alive!

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
  * 
  * @link http://en.wikipedia.org/wiki/Fizz_buzz
  * @link http://lmgtfy.com/?q=fizz+buzz
+ *       
+ * @todo Make good impression
  * 
  * @param  integer $min     The number at which to start
  * @param  integer $max     The number at which to stop
@@ -15,8 +17,45 @@
  * @return Nothing to see here.
  * 
  */
-function fizzbuzz( $min = 1, $max = 100, $options = array() ) {
+function fizzbuzz( $options = array() ) {
 
+	$defaults = array(
+		'min' => 1,
+		'max' => 100,
+		'fizznumber' => 3,
+		'buzznumber' => 5,
+		'fizz' => 'Fizz',
+		'buzz' => 'Buzz',
+		'delimiter' => ', '
+	);
 
+	$options = array_merge( $defaults, (array) $options );
+
+	extract( $options );
+
+	$results = array();
+	$count = $min;
+
+	while ( $count <= $max ) {
+
+		$output = "";
+
+		if ( ($count % $fizznumber) === 0 ) 
+			$output .= $fizz;
+
+		if ( ($count % $buzznumber) === 0 ) 
+			$output .= $buzz;
+
+		$results[] = $output ? $output : $count;
+
+		$count++;
+		
+	}
+
+	$results = implode( $delimiter, $results );
+
+	return $results;
 
 }
+
+echo fizzbuzz();
